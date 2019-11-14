7220781154706f15129da7147fb74e28bcd191d2
yu-web <54567280+yu-web@users.noreply.github.com>
2019-11-14 10:09:32 +0900

    sign_up (#2)
    * sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "staticpage#home"
+  get '/signup',to: "users#new"
+  resources :users
 end
+
+ 
+
