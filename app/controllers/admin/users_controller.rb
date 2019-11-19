f49039ff572c0da9f0c8b2966ba9c3b5d04f2e54
yu-web <54567280+yu-web@users.noreply.github.com>
2019-11-19 13:55:30 +0900

    Create admin add delete function page (#11)
    * Create admin add delete function page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,22 @@
 class Admin::UsersController < ApplicationController
   def home
+
+  end
+
+  def index
+    @users = User.paginate(page: params[:page],per_page: 7)
+  end
+  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(admin: true)
+    redirect_to admin_users_url
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.update(admin: false)
+    redirect_to admin_users_url
   end
 end
