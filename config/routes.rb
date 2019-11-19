a003e35074331a8442ee16aaa13b07a1a3e9d4b0
Yuichiro Ikeda <yibk18@gmail.com>
2019-11-19 13:39:55 +0900

    Create admin add delete function page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "staticpage#home"
   get '/signup',to: "users#new"
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get "/home", to: "users#home"
+    resources :users
   end
 
   resources :users
