24c1b964e122faf57e1505181b92ac7b0ca1aada
yu-web <54567280+yu-web@users.noreply.github.com>
2019-11-20 11:23:50 +0900

    Create category (#13)

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,21 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page],per_page: 7)
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category=Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_url
+    else
+      render "new"
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name,:content)
+  end
 end
