70a81b6dbd3e2dae3a48ddfa9512198e4cd53694
Yuichiro Ikeda <yibk18@gmail.com>
2019-11-20 11:58:41 +0900

    Created edit cate

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -16,6 +16,20 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category=Category.find(params[:id])
+  end
+
+  def update
+    @category=Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success]="Updated"
+      redirect_to admin_categories_url
+    else
+      render "edit"
+    end
+  end
+
   def category_params
     params.require(:category).permit(:name,:content)
   end
