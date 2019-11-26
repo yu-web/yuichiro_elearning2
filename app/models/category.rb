c4d482cb72f19f45afc122824d9fa5fcbc5d39fc
yu-web <54567280+yu-web@users.noreply.github.com>
2019-11-26 11:45:52 +0900

    created cate list layout (#19)
    * created cate list layout

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many:words,dependent: :destroy
-  validates :name,presence:true,length:{maximum:20}
+  validates :name,presence:true,length:{maximum:19}
   validates :content,presence:true,length:{maximum:50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions
+  resources :categories
 end
 
  
